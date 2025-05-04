a75bfc321e6db55e27d32d7f2ec244ca7ca53dfe
Мила Фатыхова <161517219+Milareina@users.noreply.github.com>
2025-05-04 16:02:28 +0300

    fix adaptiv

--- video-mask.js
+++ video-mask.js
@@ -1,35 +1,34 @@
 window.addEventListener('DOMContentLoaded', () => {
   function updateMask() {
     const svg = document.getElementById('video-mask-svg');
+    const container = document.querySelector('.container');
     const rects = document.querySelectorAll('.video-rect');
     svg.innerHTML = '';
     svg.setAttribute('width', window.innerWidth);
-    svg.setAttribute('height', window.innerHeight);
+    svg.setAttribute('height', container.offsetHeight);
 
-    
     const mask = document.createElementNS('http://www.w3.org/2000/svg', 'mask');
     mask.setAttribute('id', 'mask-rects');
 
-    
-    const white = document.createElementNS('http://www.w3.org/2000/svg', 'rect');
-    white.setAttribute('x', 0);
-    white.setAttribute('y', 0);
-    white.setAttribute('width', window.innerWidth);
-    white.setAttribute('height', window.innerHeight);
-    white.setAttribute('fill', 'black'); 
-    mask.appendChild(white);
+    const black = document.createElementNS('http://www.w3.org/2000/svg', 'rect');
+    black.setAttribute('x', 0);
+    black.setAttribute('y', 0);
+    black.setAttribute('width', window.innerWidth);
+    black.setAttribute('height', container.offsetHeight);
+    black.setAttribute('fill', 'black');
+    mask.appendChild(black);
 
-    
     rects.forEach(rect => {
       const r = rect.getBoundingClientRect();
+      const containerRect = container.getBoundingClientRect();
       const svgRect = document.createElementNS('http://www.w3.org/2000/svg', 'rect');
       
-      svgRect.setAttribute('x', r.left);
-      svgRect.setAttribute('y', r.top);
+      svgRect.setAttribute('x', r.left - containerRect.left);
+      svgRect.setAttribute('y', r.top - containerRect.top);
       svgRect.setAttribute('width', r.width);
       svgRect.setAttribute('height', r.height);
       svgRect.setAttribute('rx', 6);
-      svgRect.setAttribute('fill', 'white'); 
+      svgRect.setAttribute('fill', 'white');
       mask.appendChild(svgRect);
     });
 
@@ -40,5 +39,3 @@ window.addEventListener('DOMContentLoaded', () => {
   window.addEventListener('resize', updateMask);
   window.addEventListener('scroll', updateMask);
 });
- 
-   
